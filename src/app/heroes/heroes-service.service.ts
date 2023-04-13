4788a4d68c50a162888114f007a8105b50a67131
mcjimenez@hiberus.com <mcjimenez@hiberus.com>
2023-04-13 08:56:55 +0200

    ADD service heroes/heroe http

--- src/app/heroes/heroes-service.service.ts
+++ src/app/heroes/heroes-service.service.ts
@@ -1,10 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HeroeModel } from '../entity/heroe.entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesServiceService {
 
-  constructor() { }
+  private url = 'http://localhost:3000/';
+
+  constructor(private http: HttpClient) { }
+
+  createHeroe(heroe: HeroeModel){
+    return this.http.post<HeroeModel>(`${this.url}heroes`, heroe);
+  }
+
+  getAllHeroes(){
+    return this.http.get<HeroeModel[]>(`${this.url}heroes`);
+  }
+
+  getHeroeById(id: string){
+    return this.http.get<HeroeModel[]>(`${this.url}heroes/${id}`);
+  }
+
+  updateHeroe(heroe: HeroeModel) {
+    return this.http.put<HeroeModel>(`${this.url}heroes/${heroe.id}`, heroe);
+  }
+
+  deleteHeroe(id: string) {
+    return this.http.delete<HeroeModel>(`${this.url}heroes/${id}`);
+  }
 }
 
--- src/app/heroes/views/heroes/heroes.module.ts
+++ src/app/heroes/views/heroes/heroes.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeroesComponent } from './heroes.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+// import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
 
-import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+
+
 import { AppRoutingModule } from 'src/app/app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -22,8 +29,12 @@ import { AppRoutingModule } from 'src/app/app-routing.module';
     MatSlideToggleModule,
     MatCardModule,
     MatButtonModule,
+    //MatTableModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatListModule,
+    MatIconModule,
+    MatPaginatorModule
 
   ]
 })
