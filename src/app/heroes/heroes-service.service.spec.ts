31bbabc187a353977c36721f7054374f256dbe1a
mcjimenez@hiberus.com <mcjimenez@hiberus.com>
2023-04-18 18:13:19 +0200

    UPDATE config test

--- src/app/heroes/heroes-service.service.spec.ts
+++ src/app/heroes/heroes-service.service.spec.ts
@@ -1,16 +1,16 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { HeroesServiceService } from './heroes-service.service';
+import { HeroesServiceService } from './heroes-service.service';
 
-// describe('HeroesServiceService', () => {
-//   let service: HeroesServiceService;
+describe('HeroesServiceService', () => {
+  let service: HeroesServiceService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(HeroesServiceService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(HeroesServiceService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
